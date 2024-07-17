fcd9a08118340456a570cdee134ff7dabd1afede
Mateo Alvarado <matiw.alva172@outlook.es>
2024-07-16 23:26:12 -0500

    Update sync_projects.dart

--- lib/utils/sync/sync_projects.dart
+++ lib/utils/sync/sync_projects.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:mysql1/mysql1.dart';
 import 'package:taskermg/controllers/maincontroller.dart';
-import 'package:taskermg/controllers/project_controller.dart';
 import 'package:taskermg/db/db_helper.dart';
 import 'package:taskermg/db/db_local.dart';
 import 'package:taskermg/models/project.dart';
@@ -42,18 +40,19 @@ class SyncProjects {
       if(localProjects.isEmpty) {
         AppLog.d("No hay proyectos locales.");
       } else {
-      var localProjectIDs = localProjects.map((project) => project['projectID']).toList();
+        var localProjectIDs = localProjects.map((project) => project['projectID']).toList();
 
-      // Detect deleted projects
-      for (var localProjectID in localProjectIDs) {
-        if (!remoteProjects.contains(localProjectID)) {
-          await LocalDB.rawDelete(
-            "DELETE FROM project WHERE projectID = ?",
-            [localProjectID],
-          );
-          AppLog.d("Proyecto con ID $localProjectID marcado como eliminado.");
+        // Detect deleted projects
+        for (var localProjectID in localProjectIDs) {
+          if (!remoteProjects.contains(localProjectID)) {
+            await LocalDB.rawDelete(
+              "DELETE FROM project WHERE projectID = ?",
+              [localProjectID],
+            );
+            AppLog.d("Proyecto con ID $localProjectID marcado como eliminado.");
+          }
         }
-      }}
+      }
 
       for (var projectMap in result) {
         var projectMapped = Project(
@@ -76,17 +75,39 @@ class SyncProjects {
 
   static Future<void> pushProjects() async {
     try {
-
       var unsyncedProjects = await LocalDB.queryUnsyncedProjects();
       AppLog.d("Proyectos sin sincronizar: ${jsonEncode(unsyncedProjects)}");
       for (var projectMap in unsyncedProjects) {
-        await handleRemoteProjectInsert(projectMap);
+        var hasDeletion = await hasDeletionLog(projectMap['projectID'] ?? projectMap['locId']);
+        if (!hasDeletion) {
+          var creationActivity = await getCreationActivityByProjectID(projectMap['projectID'] ?? projectMap['locId']);
+          if (creationActivity != null) {
+            if (creationActivity['isSynced'] == 0) {
+              await handleRemoteProjectInsert(projectMap);
+            }
+          }
+        } else {
+          await markActivityLogAsSyncedByProjectId(projectMap['projectID'] ?? projectMap['locId']);
+        }
       }
 
       var unsyncedProjectUpdates = await LocalDB.queryUnsyncedUpdates('project');
       AppLog.d("Proyectos sin actualizar: ${jsonEncode(unsyncedProjectUpdates)}");
       for (var actMap in unsyncedProjectUpdates) {
-        await handleRemoteProjectUpdate(actMap);
+        var details = jsonDecode(actMap['activityDetails']);
+        var hasDeletion = await hasDeletionLog(details['projectID'] ?? details['locId']);
+        if (!hasDeletion) {
+          var creationActivity = await getCreationActivityByProjectID(details['projectID'] ?? details['locId']);
+          if (creationActivity != null) {
+            if (creationActivity['isSynced'] == 0) {
+              await handleRemoteProjectUpdate(actMap);
+            }
+          } else {
+            await handleRemoteProjectUpdate(actMap);
+          }
+        } else {
+          await markActivityLogAsSyncedByProjectId(details['projectID']);
+        }
       }
 
       var unsyncedDeletions = await LocalDB.queryUnsyncedDeletions('project');
@@ -99,6 +120,23 @@ class SyncProjects {
     } catch (e) {
       AppLog.e("Error al enviar proyectos: $e");
     }
+  }
+
+  //get creation activity by projectID
+  static Future<Map<String, dynamic>?> getCreationActivityByProjectID(
+      int projectID) async {
+    var activities = await LocalDB.rawQuery(
+      "SELECT * FROM activityLog WHERE activityType = 'create'",
+    );
+    for (Map<String, dynamic> activity in activities) {
+      var details = jsonDecode(activity['activityDetails']);
+
+      var actProjectId = details['locId'];
+      if (details['table'] == 'project' && actProjectId == projectID) {
+        return activity;
+      }
+    }
+    return null;
   }
 
   static Future<void> handleProjectSync(Map<String, dynamic> projectMap) async {
@@ -139,7 +177,7 @@ class SyncProjects {
 
   static Future<void> handleRemoteProjectUpdate(Map<String, dynamic> actMap) async {
     var actDetails = jsonDecode(actMap['activityDetails']);
-    var projectMap = Map<String, dynamic>();
+    var projectMap = <String, dynamic>{};
     //get projectMap from activityDetails and localdb 
     if(actDetails['projectID'] != null){
       projectMap = (await LocalDB.queryProjectByRemoteID(actDetails['projectID']))!;
@@ -154,30 +192,103 @@ class SyncProjects {
     String creationDate = formatDateTime(DateTime.parse(projectMap['creationDate']));
     String lastUpdate = formatDateTime(DateTime.parse(projectMap['lastUpdate']));
 
-    var response;
-    response = await DBHelper.query(
+    //check last update in remote and update only if local is newer
+    var remoteProject = await DBHelper.query(
+      "SELECT * FROM project WHERE projectID = ?",
+      [projectMap['projectID']],
+    );
+    if (remoteProject.isNotEmpty) {
+      var remoteLastUpdate = remoteProject.first['lastUpdate'];
+
+      var localLastUpdate = DateTime.parse(projectMap['lastUpdate']);
+      if (remoteLastUpdate.isAfter(localLastUpdate)) {
+        AppLog.d("Proyecto remoto más reciente, no se actualizará.");
+        return;
+      }
+    }
+
+    var response = await DBHelper.query(
       "UPDATE project SET name = ?, description = ?, deadline = ?, proprietaryID = ?, lastUpdate = ? WHERE projectID = ?",
       [name, description, deadline, proprietaryID, lastUpdate, projectMap['projectID']],
     );
 
     if (response is Results) {
-      var insertId = response.insertId;
-      if (insertId != null) {
-        await LocalDB.markActivityLogAsSynced(projectMap['locId']);
-      }
+      await LocalDB.markActivityLogAsSynced(actMap['locId']);
     } else {
-      AppLog.e("Error inserting/updating project in remote database: $response");
+      AppLog.e("Error updating project in remote database: $response");
     }
   }
 
   static Future<void> handleRemoteProjectDeletion(Map<String, dynamic> deletion) async {
     var activityDetails = jsonDecode(deletion['activityDetails']);
     var remoteID = activityDetails['projectID'] ?? activityDetails['locId'];
-    await DBHelper.query(
-      "DELETE FROM project WHERE projectID = ?",
+    // Verificar si el proyecto existe en la base de datos remota
+    var existProject = await DBHelper.query(
+      "SELECT * FROM project WHERE projectID = ?",
       [remoteID],
     );
+    var projectMap = existProject.isNotEmpty ? existProject.first : null;
+    if (projectMap != null) {
+      await DBHelper.query(
+        "DELETE FROM project WHERE projectID = ?",
+        [remoteID],
+      );
+    }
     var delLocId = deletion['locId'];
     await LocalDB.markActivityLogAsSynced(delLocId);
+  }
+
+  static Future<bool> hasDeletionLog(int projectId) async {
+    var deletions = await LocalDB.queryUnsyncedDeletions('project');
+    for (var deletion in deletions) {
+      var details = jsonDecode(deletion['activityDetails']);
+      if (details['projectID'] == projectId || details['locId'] == projectId) {
+        //establecer a la actividad de creacion como isSynced
+        var creationActivity = await getCreationActivityByProjectID(projectId);
+        if (creationActivity != null) {
+          await LocalDB.markActivityLogAsSynced(creationActivity['locId']);
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static Future<void> markActivityLogAsSyncedByProjectId(int projectId) async {
+    // Marcar actividades de creación como sincronizadas
+    var activities = await LocalDB.queryUnsyncedActivityLogs();
+    // Filtrar actividades de creación y actualizacion
+    var filAct = [];
+    for (var act in activities) {
+      if (act['activityType'] == 'create' || act['activityType'] == 'update') {
+        filAct.add(act);
+      }
+    }
+
+    for (var activity in filAct) {
+      var data = jsonDecode(activity['activityDetails']);
+      if (data['projectID'] == projectId || data['locId'] == projectId) {
+        var updated = await LocalDB.markActivityLogAsSynced(activity['locId']);
+        AppLog.d("Actividad de creación marcada como sincronizada: $updated");
+      }
+    }
+
+    // Marcar actividades de actualización como sincronizadas
+    var updates = await LocalDB.queryUnsyncedUpdates('project');
+    for (var update in updates) {
+      var details = jsonDecode(update['activityDetails']);
+      if (details['projectID'] == projectId || details['locId'] == projectId) {
+        await LocalDB.markActivityLogAsSynced(update['locId']);
+      }
+    }
+
+    // Marcar actividades de eliminación como sincronizadas
+    var deletions = await LocalDB.queryUnsyncedDeletions('project');
+    for (var deletion in deletions) {
+      var details = jsonDecode(deletion['activityDetails']);
+      if (details['projectID'] == projectId || details['locId'] == projectId) {
+        await LocalDB.markActivityLogAsSynced(deletion['locId']);
+      }
+    }
   }
 }
